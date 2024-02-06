181c892056ee4f38c9f4a9bf0c2ae4aa3473daca
modemnaphat <modemnaphat@gmail.com>
2024-02-06 12:14:57 +0700

    add file

---
+++ src/page/status.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/navbar"
+import Sidebar from "./components/sidebar"
+
+function status() {
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+    </>
+  )
+}
+
+export default status
